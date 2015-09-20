bcfcafb0a21ae867b0e367f096a97cb702bb1f9e
Pasha Pashchenko <Pasha@Pashas-Mac-mini.local>
2015-09-20 04:10:09 -0700

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Pasha Pashchenko on 9/20/15.
+//  Copyright (c) 2015 Pasha Pashchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by Pasha Pashchenko on 9/20/15.
+//  Copyright (c) 2015 Pasha Pashchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
